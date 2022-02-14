35fc6b9ef7cf186c5613332469ff5f97ddf457e6
hoonjoo94 <hoonjoo94@naver.com>
2022-02-15 03:17:25 +0900

    ✨ 캐러셀 드래그 슬라이드 기능 구현 완료

--- src/components/carousel/CarouselButton.tsx
+++ src/components/carousel/CarouselButton.tsx
@@ -1,12 +1,36 @@
 import { CAROUSEL_IMAGES, COLOR } from '@constants';
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
 
-export const CarouselButton = () => {
+interface Props {
+  currentIndex: number;
+  setCurrentIndex: Dispatch<SetStateAction<number>>;
+}
+
+export const CarouselButton = ({ currentIndex, setCurrentIndex }: Props) => {
+  let index: number = currentIndex;
+  useEffect(() => {
+    const maxCarouselLength = CAROUSEL_IMAGES.length - 1;
+    if (currentIndex === 0) {
+      index = 1;
+      return;
+    } else if (currentIndex === maxCarouselLength) {
+      index = maxCarouselLength - 1;
+      return;
+    }
+    index = currentIndex;
+  }, [currentIndex]);
+  const handleClick = (i: number) => {
+    setCurrentIndex(i);
+  };
   return (
     <ButtonUl>
       {CAROUSEL_IMAGES.map((image, i) => (
-        <IndexButton key={`button-${i}`}></IndexButton>
+        <IndexButton
+          key={`button-${i}`}
+          className={i + 1 === index ? 'active' : ''}
+          onClick={() => handleClick(i + 1)}
+        ></IndexButton>
       ))}
     </ButtonUl>
   );
@@ -29,4 +53,7 @@ const IndexButton = styled.button`
   background-color: ${COLOR.WHITE};
   border-radius: 50%;
   cursor: pointer;
+  &.active {
+    background-color: ${COLOR.BLACK};
+  }
 `;
