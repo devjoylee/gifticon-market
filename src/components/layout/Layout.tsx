2d78351937c56ff1b48ea0719369cbec118753bf
HyunHo Lee <dlgusgh2001@naver.com>
2022-02-14 19:37:13 +0900

    💄전체 UI 크기 셋팅

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,16 +4,24 @@ import {
   SearchOutlined,
   CreditCardOutlined,
 } from '@ant-design/icons';
+import { COLOR } from '@constants';
 
 export const Layout: React.FC = ({ children }) => {
   return (
-    <div>
+    <Container>
       <div>
         <MenuOutlined />
         <SearchOutlined />
         <CreditCardOutlined />
       </div>
       <main>{children}</main>
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 672px;
+  width: 100vw;
+  background-color: ${COLOR.WHITE};
+`;
