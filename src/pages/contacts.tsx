2b22f4a35c14deb50e80ba7314dfd8040bd10df4
HyunHo Lee <dlgusgh2001@naver.com>
2022-02-16 00:12:02 +0900

    ✨next swr 에서 SSG 기능 구현

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,17 +1,36 @@
 import { QnAContainer } from '@components/QnA';
-import { useData } from '@hooks/useData';
-import { NextPage } from 'next';
+import { API_ENDPOINT } from '@constants';
+import { fetcher } from '@utils/fetcher';
+import { GetStaticProps, NextPage } from 'next';
+import useSWR, { SWRConfig } from 'swr';
 
-const ContactsPage: NextPage = () => {
-  const { data: QnATypeData, error: QnATypeError } = useData('qa-types');
-  if (!QnATypeData) return <div>로딩중..</div>;
-  if (QnATypeError) return <div>페이지 에러</div>;
-
+const ContactsPage: NextPage = ({ fallback }: any) => {
   return (
-    <div>
-      <QnAContainer QnATypeData={QnATypeData.qaTypes} />
-    </div>
+    <SWRConfig value={{ fallback }}>
+      <QnAType />
+    </SWRConfig>
   );
 };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const QnATypeData = await fetcher(`${API_ENDPOINT}/qa-types`);
+  return {
+    props: {
+      fallback: {
+        [`${API_ENDPOINT}/qa-types`]: QnATypeData,
+      },
+    },
+  };
+};
+
+function QnAType() {
+  const { data: QnATypeData, error: QnATypeError } = useSWR(
+    `${API_ENDPOINT}/qa-types`
+  );
+  if (!QnATypeData) return <div>로딩</div>;
+  if (QnATypeError) return <div>에러</div>;
+
+  return <QnAContainer QnATypeData={QnATypeData.qaTypes} />;
+}
 
 export default ContactsPage;
