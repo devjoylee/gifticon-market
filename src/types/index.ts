8904fd2fe2280bd4383c7e256913482dcbde7ffa
hoonjoo94 <hoonjoo94@naver.com>
2022-02-15 14:22:54 +0900

    🚑️ conflict 해결

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,3 @@
 export * from './QnAType';
-<<<<<<< HEAD
 export * from './ItemType';
 export * from './CategoryType';
-=======
-export * from './itemsType';
->>>>>>> 554885f (💄 items 페이지 마크업 및 UI 구현)
