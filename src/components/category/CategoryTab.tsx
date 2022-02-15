d8704590decfc9311aaba1e0c34510c9b101a71c
hoonjoo94 <hoonjoo94@naver.com>
2022-02-16 02:41:32 +0900

    ✨ 카테고리 탭 위치에 따른 동적 scrollLeft 구현

--- src/components/category/CategoryTab.tsx
+++ src/components/category/CategoryTab.tsx
@@ -1,47 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useEffect, useState } from 'react';
 import { useData } from '@hooks/useData';
 import { Category } from '@types';
-import { COLOR } from '@constants';
+import { COLOR, STYLE } from '@constants';
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import { TabButton } from './TabButton';
 
 interface Props {
   id: string | string[];
 }
 
 export const CategoryTab = ({ id }: Props) => {
+  const [toMove, setToMove] = useState(0);
   const { data } = useData(`con-category1s`);
   const [categories, setCategories] = useState<Category[]>();
-
+  const sectionRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     data && setCategories(data.conCategory1s);
   }, [data]);
-
+  useEffect(() => {
+    if (sectionRef.current) {
+      sectionRef.current.scrollLeft = toMove;
+    }
+  }, [sectionRef, toMove]);
   return (
-    <TabSection>
+    <TabSection ref={sectionRef}>
       {categories &&
-        categories.map((category: Category) => (
-          <Link href={`/categories/${category.id}`} key={category.id} passHref>
-            <TabName className={`${category.id}` === `${id}` ? 'active' : ''}>
-              {category.name}
-            </TabName>
-          </Link>
+        categories.map((category: Category, i) => (
+          <TabButton
+            key={`tab-${i}`}
+            category={category}
+            idx={i}
+            id={id}
+            setToMove={setToMove}
+          />
         ))}
     </TabSection>
   );
 };
-
-const TabName = styled.button`
-  padding: 10px 14px;
-  height: 41px;
-  border-bottom: 1px solid ${COLOR.GRAY_BORDER};
-  cursor: pointer;
-  &.active {
-    color: ${COLOR.RED};
-    border-bottom: 3px solid ${COLOR.RED};
-  }
-`;
 
 const TabSection = styled.div`
   display: flex;
---
+++ src/components/category/TabButton.tsx
@@ -0,0 +1,41 @@
+import React, { Dispatch, SetStateAction, useEffect, useRef } from 'react';
+import Link from 'next/link';
+import { Category } from '@types';
+import styled from '@emotion/styled';
+import { COLOR } from '@constants';
+
+interface Props {
+  category: Category;
+  id: string | string[];
+  idx: number;
+  setToMove: Dispatch<SetStateAction<number>>;
+}
+
+export const TabButton = ({ category, id, setToMove, idx }: Props) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const isCurrent = category.id.toString() === id;
+  useEffect(() => {
+    if (buttonRef.current && isCurrent) {
+      const distance = idx * 60;
+      setToMove(distance);
+    }
+  }, []);
+  return (
+    <Link href={`/categories/${category.id}`} key={category.id} passHref>
+      <TabName className={isCurrent ? 'active' : ''} ref={buttonRef}>
+        {category.name}
+      </TabName>
+    </Link>
+  );
+};
+
+const TabName = styled.button`
+  padding: 10px 14px;
+  height: 41px;
+  border-bottom: 1px solid ${COLOR.GRAY_BORDER};
+  cursor: pointer;
+  &.active {
+    color: ${COLOR.RED};
+    border-bottom: 3px solid ${COLOR.RED};
+  }
+`;
