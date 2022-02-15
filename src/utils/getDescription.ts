f2a7d3d32b2608bddc0039789c3ce51b4c6491c2
hoonjoo94 <hoonjoo94@naver.com>
2022-02-16 01:02:39 +0900

    ♻️ 상품 설명에서'*금액권일 경우'의 텍스트도 잘 적용되도록 구현

--- src/utils/getDescription.ts
+++ src/utils/getDescription.ts
@@ -4,10 +4,13 @@ export const getDescription = async (desc: string) => {
   let descriptions: Description[] = [];
   const sliceRestricted = desc && desc.replace('\n[사용 불가 매장]\n', '');
   const splitDesc = sliceRestricted.split('\n\n');
-  const labelRegex = new RegExp('\\[.*\\n', 'g');
+  const normalRegex = new RegExp('\\[.*\\n', 'g');
+  const voucherRegex = new RegExp('\\*.*\\n', 'g');
   splitDesc.forEach((text, i) => {
-    const rawLabel = text.match(labelRegex)![0];
-    const labelString = rawLabel.slice(1, rawLabel.length - 2);
+    const rawLabel = text.match(normalRegex)
+      ? text.match(normalRegex)![0]
+      : text.match(voucherRegex)![0];
+    const labelString = rawLabel.replace('[', '').replace(']', '');
     const descText = text.slice(rawLabel.length).replaceAll('-', '·');
     const newDesc = { label: labelString, text: descText };
     descriptions.push(newDesc);
